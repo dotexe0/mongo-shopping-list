f4e9b1d08f653a42252e517f3153c2903ee84663
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-12-10 23:42:28 -0800

    created db with mlab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'ds127878.mlab.com:27878/shopping-list-db -u dotexe0 -p Squier101!' :
+                            'mongodb://<dotexe0>:<Squier101!>@ds127878.mlab.com:27878/shopping-list-db':
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
