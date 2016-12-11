f3e30eb0f339d2944e43debabfb8435b36c7ab0b
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-12-11 10:46:47 -0800

    changed usr/pwd for db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://guest:password@ds127878.mlab.com:27878/shopping-list-db':
+                            'mongodb://<guest>:<password>@ds127878.mlab.com:27878/shopping-list-db':
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
