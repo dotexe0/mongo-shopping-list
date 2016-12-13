86eb1a09589271c0bd324d1ca53f77cbccd011d6
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-12-12 16:00:45 -0800

    added usr/pw to config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://<guest>:<password>@ds127878.mlab.com:27878/shopping-list-db':
-                            'mongodb://localhost/shopping-list-dev');
+                       'mongodb://dan:dantheman@ds127878.mlab.com:27878/shopping-list-db':
+                       'mongodb://<dbuser>:<dbpassword>@ds127878.mlab.com:27878/shopping-list-db');
+                       //'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
