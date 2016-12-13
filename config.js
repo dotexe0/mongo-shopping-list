2b85f3ff41a69aa729898d7d6c1a9b11bb6793cf
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-12-12 16:27:05 -0800

    added new usr

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                       'mongodb://dotexe0:Squier101!@ds127878.mlab.com:27878/shopping-list-db':
-                       'mongodb://<dbuser>:<dbpassword>@ds127878.mlab.com:27878/shopping-list-db');
+                       'mongodb://guest:password@ds127878.mlab.com:27878/shopping-list-db':
+                       'mongodb://dan:dantheman@ds127878.mlab.com:27878/shopping-list-db');
                        //'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
